6bf090467cb599ee774d0342142a9fcf5541122d
nas- <nas-@users.noreply.github.com>
2021-12-27 16:35:05 +0100

    Added imports

--- costum_transformers.py
+++ costum_transformers.py
@@ -1,34 +1,43 @@
-from sklearn.base import BaseEstimator,TransformerMixin
+from sklearn.base import BaseEstimator, TransformerMixin
+import pandas as pd
+import numpy as np
 
-class DegreesValuesFixer(BaseEstimator,TransformerMixin):
-    #trasforms values in degrees in their sinus.
-    #Useful because 365 is far from 0, but sin365 is close to sin0
+
+class DegreesValuesFixer(BaseEstimator, TransformerMixin):
+    # trasforms values in degrees in their sinus.
+    # Useful because 365 is far from 0, but sin365 is close to sin0
     def __init__(self):
         pass
-    def fit(self,X,y=None):
+
+    def fit(self, X, y=None):
         return self
-    def transform(self,X):
+
+    def transform(self, X):
         # we change all values to radiants and convert to the sinus
-        #X[X<0]+=360
-        #X[X>=360]-=360
-        X=np.sin(np.radians(X))
+        # X[X<0]+=360
+        # X[X>=360]-=360
+        X = np.sin(np.radians(X))
         return X
 
-class SubValueTrimmer(BaseEstimator,TransformerMixin):
-    def __init__(self,min_val,max_val):
-        #trasforms values that are higher than max to max and less then min to min
-        self.min_val=min_val
-        self.max_val=max_val
-    def fit(self,X,y=None):
+
+class SubValueTrimmer(BaseEstimator, TransformerMixin):
+    def __init__(self, min_val, max_val):
+        # trasforms values that are higher than max to max and less then min to min
+        self.min_val = min_val
+        self.max_val = max_val
+
+    def fit(self, X, y=None):
         return self
 
-    def transform(self,X):
-        X[X<self.min_val]=self.min_val
-        X[X>self.max_val]=self.max_val
+    def transform(self, X):
+        X[X < self.min_val] = self.min_val
+        X[X > self.max_val] = self.max_val
         return X
+
 
 from sklearn.utils import shuffle
 from sklearn.base import BaseEstimator, RegressorMixin
+
 
 class PseudoLabeler(BaseEstimator, RegressorMixin):
     """
@@ -57,6 +66,7 @@ class PseudoLabeler(BaseEstimator, RegressorMixin):
     #  score_cv=score_description
     #  ))
     """
+
     def __init__(self, model, unlabled_data, features, target, sample_rate=0.2, seed=42):
         """
         @sample_rate - percent of samples used as pseudo-labelled data
@@ -74,19 +84,19 @@ class PseudoLabeler(BaseEstimator, RegressorMixin):
         self.target = target
 
     def get_params(self, deep=True):
-         return {
-         "sample_rate": self.sample_rate,
-         "seed": self.seed,
-         "model": self.model,
-         "unlabled_data": self.unlabled_data,
-         "features": self.features,
-         "target": self.target
-         }
+        return {
+            "sample_rate": self.sample_rate,
+            "seed": self.seed,
+            "model": self.model,
+            "unlabled_data": self.unlabled_data,
+            "features": self.features,
+            "target": self.target
+        }
 
     def set_params(self, **parameters):
-         for parameter, value in parameters.items():
+        for parameter, value in parameters.items():
             setattr(self, parameter, value)
-         return self
+        return self
 
     def fit(self, X, y):
         """
@@ -94,8 +104,8 @@ class PseudoLabeler(BaseEstimator, RegressorMixin):
         """
         augemented_train = self.__create_augmented_train(X, y)
         self.model.fit(
-        augemented_train[self.features],
-        augemented_train[self.target]
+            augemented_train[self.features],
+            augemented_train[self.target]
         )
         return self
 
@@ -115,7 +125,7 @@ class PseudoLabeler(BaseEstimator, RegressorMixin):
         pseudo_data[self.target] = pseudo_labels
 
         # Take a subset of the test set with pseudo-labels and append in onto
-         # the training set
+        # the training set
         sampled_pseudo_data = pseudo_data.sample(n=num_of_samples)
         temp_train = pd.concat([X, y], axis=1)
         augemented_train = pd.concat([sampled_pseudo_data, temp_train])
@@ -129,4 +139,3 @@ class PseudoLabeler(BaseEstimator, RegressorMixin):
 
     def get_model_name(self):
         return self.model.__class__.__name__
-
